009a1bf5508ad62eecab868ff0bbe0211a7988f9
Kaue Martins <kaue.martins124@gmail.com>
2024-01-02 15:02:16 -0400

    definindo classes

---
+++ Avaliação2/src/models/Filme.ts
@@ -0,0 +1,16 @@
+class Filme{
+    private titulo: string;
+    private genero: string;
+    private duracao: number;
+    private rating: number;
+    private preco: number;
+
+    constructor(titulo: string, genero: string, duracao: number, rating: number, preco: number){
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.rating = rating;
+        this.preco = preco;
+    }
+
+}
